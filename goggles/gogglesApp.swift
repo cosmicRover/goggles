8c63dfe3af3c843b46f9dbd809111e94529eaffb
Joy Paul <jpaul3250@gmail.com>
2024-04-05 13:08:25 -0400

    Initial Commit

---
+++ goggles/gogglesApp.swift
@@ -0,0 +1,17 @@
+//
+//  gogglesApp.swift
+//  goggles
+//
+//  Created by Joy Paul on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct gogglesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
